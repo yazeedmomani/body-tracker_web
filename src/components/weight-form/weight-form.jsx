726fd003d877b72bf6d9166ab8089211c35a5835
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-04-03 21:14:08 +0300

    create weight form

--- src/components/weight-form/weight-form.jsx
+++ src/components/weight-form/weight-form.jsx
@@ -1,5 +1,43 @@
+import { useState } from "react";
 import styles from "./weight-form.module.scss";
 
 export default function WeightForm() {
-  return <h1>Weight Form</h1>;
+  const [date, setDate] = useState();
+  const [weight, setWeight] = useState();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!date || !weight) return;
+
+    const newDate = new Date(date);
+    const dateString = newDate.toString();
+
+    console.log(dateString, +weight);
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className={styles.form}>
+      <input
+        className={styles.form_input}
+        type="date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+      />
+      <input
+        className={styles.form_input}
+        type="number"
+        placeholder="Enter Weight"
+        value={weight}
+        onChange={(e) => setWeight(e.target.value)}
+      />
+      <button
+        type="submit"
+        className={styles.form_btn}>
+        Submit
+      </button>
+    </form>
+  );
 }
